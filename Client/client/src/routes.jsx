f4704e056239666af3f9ee8f18735608d412d0bd
saramalka <sari.shlomovitz@gmail.com>
2025-07-14 00:24:21 +0300

    delete marks

--- Client/client/src/routes.jsx
+++ Client/client/src/routes.jsx
@@ -1,22 +1,8 @@
 import { useState } from "react";
-import  Chat from './chatBot_components/Chat'
 import  Login from './chatBot_components/Login'
-import AdminQuickRepliesPage from "./chatBot_components/AdminQuickRepliesPage";
 import { Routes, Route, Navigate } from "react-router-dom";
 import UsersPage from "./chatBot_components/UsersPage";
 import NutritionComponent from "./chatBot_components/Nutrition";
-
-// import LoginForm from "./Components/login";
-// import SignUpForm from "./Components/register";
-// import Dashboard from "./Components/dashboard";
-// import TaskDetails from "./Components/taskDetails";
-// import Profile from "./Components/profile";
-// import TaskList from "./Components/taskList";
-// import TeamsList from './Components/teamsList'
-// import Homepage from "./Components/homepage";
-// import UserList from "./Components/userList";
-
-
 
 const AppRoutes = () => {
   const [login,setLogin]=useState(false)
@@ -30,17 +16,7 @@ const AppRoutes = () => {
     <Routes>
      <Route path="/chat" element={<NutritionComponent />} />
        <Route path="/" element={<Login/>}/> 
-       <Route path="/admin/replies" element={<AdminQuickRepliesPage />} />
        <Route path="/admin/users" element={<UsersPage />} />
-      {/* <Route path="/login" element={<LoginForm onLogin={handleLogin}/>} />
-      <Route path="/register" element={<SignUpForm onLogin={handleLogin}/>} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/" element={<Homepage />} />
-      <Route path="/teams" element={<TeamsList />} />
-      <Route path="/tasks" element={<TaskList />} />
-      
-      <Route path="profile" element={<Profile />} /> */}
-  
     </Routes>
   );
 };
--- Server/models/HealthDataUser.js
+++ Server/models/HealthDataUser.js
@@ -1,15 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const healthDataSchema = new mongoose.Schema({
-//   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', unique: true },
-//   weight: Number,
-//   height: Number,
-//   age: Number,
-//   gender: String,
-//   allergies: [String] // אם אתה מקבל כטקסט, תפרק אותו קודם
-// });
-
-// module.exports = mongoose.model('HealthDataUser', healthDataSchema);
 
 const mongoose = require('mongoose');
 
